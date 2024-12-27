3c410262747bc8be252f4b4c0b08c9a90cd95699
Benben599 <bengadi.o@hotmail.com>
2024-12-27 15:32:21 +0100

    feat: create class and instance

---
+++ index.js
@@ -0,0 +1,20 @@
+class Voiture {
+    constructor(marque, modele, annee, couleur) {
+        this.marque = marque;
+        this.modele = modele;
+        this.annee = annee;
+        this.couleur = couleur;
+    }
+
+    afficheDetails() {
+        console.log(this.marque)
+        console.log(this.modele)
+        console.log(this.annee)
+        console.log(this.couleur)
+        return Voiture
+    }
+}
+
+const voiture = new Voiture("Toyota", "Yaris", "2002", "Rouge")
+
+voiture.afficheDetails();
